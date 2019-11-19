38d63662190f7a021a8d2c2dccd29af83839f8ac
chencangui <1130285003@qq.com>
2019-11-20 07:57:11 +0800

    常见错误异常

--- NDKDemo4/app/src/main/cpp/native-lib.cpp
+++ NDKDemo4/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,33 @@
 #include <jni.h>
 #include <string>
 
+//空指针异常
+void nullPointCrash() {
+    int *p = NULL;
+    *p = 1;
+    printf("%d\n", *p);
+}
+
+//野指针
+void errorAddressCrash() {
+    //没有分配地址
+    int *p;
+    //进行赋值
+    *p = 1;
+}
+
+//数据越界
+void arrayListOutBounds() {
+    int data[10];
+    data[11] = 11;
+}
+
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_canter_ndkdemo4_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    nullPointCrash();
     return env->NewStringUTF(hello.c_str());
 }
