07cec0a7967cddc751cc3c375e40032501392fae
chencangui <1130285003@qq.com>
2019-11-19 08:32:30 +0800

    init demo 4  crash

---
+++ NDKDemo4/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_canter_ndkdemo4_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
